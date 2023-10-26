bcdae8f00e06f1c591e5031d438b07b5deb41abe
chennamjaya <chennamjaya@gmail.com>
2023-10-26 16:45:19 +0100

    Added if condition

--- backend.sh
+++ backend.sh
@@ -4,34 +4,78 @@ component=backend
 
 echo Install NodeJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Install NodeJS
 dnf install nodejs -y &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Add Application User
 useradd expense &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Clean App Content
 rm -rf /app &>>$log_file
 mkdir /app 
-
 cd /app
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 download_and_extract
 
 echo Download Dependencies
 npm install &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Start Backend Service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Install MySQL Client
 dnf install mysql -y &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Load Schema
 mysql -h mysql.vaishnavidevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
--- frontend.sh
+++ frontend.sh
@@ -3,12 +3,27 @@ component=frontend
 
 echo Installing Nginx
 dnf install nginx -y &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Placing Expense Config file in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 echo Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
 
 cd /usr/share/nginx/html 
 
@@ -17,4 +32,8 @@ download_and_extract
 echo Starting Nginx code
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-
+if [ $?-eq 0 ]; then
+ echo Success
+else
+ echo Failed
+fi
