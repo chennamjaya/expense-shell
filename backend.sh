82e291a307c625bf70307fc19b840f413d541d61
chennamjaya <chennamjaya@gmail.com>
2023-10-26 17:08:48 +0100

    Updated the print color

--- backend.sh
+++ backend.sh
@@ -5,33 +5,33 @@ component=backend
 echo Install NodeJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Install NodeJS
 dnf install nodejs -y &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Add Application User
 useradd expense &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Clean App Content
@@ -39,24 +39,24 @@ rm -rf /app &>>$log_file
 mkdir /app 
 cd /app
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 download_and_extract
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Download Dependencies
 npm install &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Start Backend Service
@@ -64,23 +64,23 @@ systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Install MySQL Client
 dnf install mysql -y &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Load Schema
 mysql -h mysql.vaishnavidevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
--- frontend.sh
+++ frontend.sh
@@ -4,41 +4,41 @@ component=frontend
 echo Installing Nginx
 dnf install nginx -y &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Placing Expense Config file in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 cd /usr/share/nginx/html 
 
 download_and_extract
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
 
 echo Starting Nginx code
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
 if [ $? -eq 0 ]; then
- echo Success
+ echo -e "\e[32mSuccess\e[0m"
 else
- echo Failed
+ echo -e "\e[31mFailed\e[0m"
 fi
