6926d143957caaa39af240d171a2b2dfc778e5d8
chennamjaya <chennamjaya@gmail.com>
2023-10-27 01:28:55 +0100

    Updated the password

--- backend.sh
+++ backend.sh
@@ -47,5 +47,6 @@ dnf install mysql -y &>>$log_file
 stat_check
 
 echo Load Schema
-mysql -h mysql.vaishnavidevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+mysql_root_password=$1
+mysql -h mysql.vaishnavidevops.online -uroot -p$mysql_root_password < /app/schema/backend.sql &>>$log_file
 stat_check
